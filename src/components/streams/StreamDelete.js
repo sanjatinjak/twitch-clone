fb9e827d310b21066423455af43852e903a29a8e
sanjatinjak <tinjaksanja@gmail.com>
2021-09-06 15:04:37 +0200

    user can delete stream

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,52 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-const StreamDelete = () => {
-  return <div>Stream delete</div>;
+import Modal from "../Modal";
+import history from "../../history";
+import { fetchStream, deleteStream } from "../../actions";
+
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    fetchStream(this.props.match.params.id);
+  }
+
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(this.props.match.params.id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <Modal
+        title={
+          this.props.stream
+            ? `Delete stream ${this.props.stream.title}`
+            : "Delete stream"
+        }
+        content="Are you sure you want to delete this stream ?"
+        actions={this.renderActions()}
+        onDismiss={() => history.push("/")}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
