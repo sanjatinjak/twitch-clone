d430261d7cba965f1bb24c81a207e59aec4852bb
sanjatinjak <tinjaksanja@gmail.com>
2021-09-06 10:51:35 +0200

    reusable component form

---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+class StreamForm extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return <span style={{ color: "red" }}>{error}</span>;
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "Please enter valid title.";
+  }
+
+  if (!formValues.description) {
+    errors.description = "Please enter valid description.";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "streamForm",
+  validate,
+})(StreamForm);
